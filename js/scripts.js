5938b89fe8cfcf95ac7463c701b6a36409d424c7
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-05-06 17:36:51 +0300

    Adds img id for portfolio images and links them to jQuery for hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,45 @@
 $(document).ready(function(){
-$("#work1").hover(function() {
+	$("#work1").hover(function() {
 	$(this).animate({opacity: 0.5}, 500);
-}, function() {
+	}, function() {
 	$(this).animate({opacity: 1.0}, 500);
-});
+	});
+	$("#work2").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+	$("#work3").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+	$("#work4").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+	$("#work5").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+	$("#work6").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+	$("#work7").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+	$("#work8").hover(function() {
+	$(this).animate({opacity: 0.5}, 500);
+	}, function() {
+	$(this).animate({opacity: 1.0}, 500);
+	});
+
 });
 
 
