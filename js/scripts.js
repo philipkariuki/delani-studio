4df596d9fb087c537613a097814fcff917994cec
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-05-07 01:52:22 +0300

    Indents new lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,44 @@
 // Front-end logic
 $(document).ready(function(){
 	$("#work1").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work2").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work3").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work4").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work5").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work6").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work7").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	$("#work8").hover(function() {
-	$(this).animate({opacity: 0.5}, 500);
+		$(this).animate({opacity: 0.5}, 500);
 	}, function() {
-	$(this).animate({opacity: 1.0}, 500);
+		$(this).animate({opacity: 1.0}, 500);
 	});
 	// mouseOver display hidden div
 	$('.description-wwd').mouseover(function() {
